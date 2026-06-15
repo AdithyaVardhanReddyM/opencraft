d056081c7f96bbbae4e45cce8a5816b066235434
AdithyaVardhanReddyM <mavr10.7.004@gmail.com>
2026-06-16 03:18:28 +0530

    fix: script bug pendo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -38,15 +37,20 @@ export default function RootLayout({
       }}
     >
       <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
-        <Script id="pendo-install" strategy="beforeInteractive" dangerouslySetInnerHTML={{
-          __html: `(function(apiKey){
+        <head>
+          <script
+            id="pendo-install"
+            dangerouslySetInnerHTML={{
+              __html: `(function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track','trackAgent'];for(w=0,x=v.length;w<x;++w)(function(m){
     o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
     y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
     z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-})('22a5f0b9-9976-4d1c-a00e-33cc3e8e2f64');`
-        }} />
+})('22a5f0b9-9976-4d1c-a00e-33cc3e8e2f64');`,
+            }}
+          />
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
